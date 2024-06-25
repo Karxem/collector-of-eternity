3d7d288f5e5f37fbe06ed11f00ce3fa4df3429a9
Karxem <tippjoshua@gmx.de>
2024-06-25 17:32:46 +0200

    refactor: add IntersectAt function to rectangle

--- internal/libs/rectangle.go
+++ internal/libs/rectangle.go
@@ -24,9 +24,21 @@ func (r Rect) MaxY() float64 {
 	return r.Y + r.Height
 }
 
+// Check collision of two rectangles
 func (r Rect) Intersects(other Rect) bool {
 	return r.X <= other.MaxX() &&
 		other.X <= r.MaxX() &&
 		r.Y <= other.MaxY() &&
 		other.Y <= r.MaxY()
+}
+
+// Check collision with an fixed position
+func (r Rect) IntersectsAt(other Rect, offsetX, offsetY float64) bool {
+	adjustedRect := Rect{
+		X:      r.X + offsetX,
+		Y:      r.Y + offsetY,
+		Width:  r.Width,
+		Height: r.Height,
+	}
+	return adjustedRect.Intersects(other)
 }

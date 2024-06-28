921bf482717dc6aaa57e232ac03957d6b3da0b06
Karxem <tippjoshua@gmx.de>
2024-06-28 22:21:55 +0200

    feat: add basic colission detection to colission handler

--- internal/systems/collision_handler.go
+++ internal/systems/collision_handler.go
@@ -1,1 +1,34 @@
-package collision_handler
+package systems
+
+import (
+	"log"
+	"pick-it-up/internal/objects"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type CollsionHandler struct {
+	Entities []objects.Entity
+}
+
+func NewCollisionHandler() *CollsionHandler {
+	return &CollsionHandler{}
+}
+
+func (ch *CollsionHandler) Update(player objects.Player) error {
+	for i, m := range ch.Entities {
+		if player.BoundingBox.Intersects(m.BoundingBox) {
+			log.Println("Intersected with entity:", i)
+		}
+	}
+
+	return nil
+}
+
+func (ch *CollsionHandler) Draw(screen *ebiten.Image) {
+
+}
+
+func (ch *CollsionHandler) AddEntity(e objects.Entity) {
+	ch.Entities = append(ch.Entities, e)
+}

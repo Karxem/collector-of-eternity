177359ec1022ff449dc65bd297e4d4d78d3ecf6a
Karxem <tippjoshua@gmx.de>
2024-06-28 22:21:55 +0200

    feat: add basic colission detection to colission handler

--- assets.go
+++ assets.go
@@ -21,17 +21,17 @@ var (
 
 func init() {
 	PlayerAnimations = *libs.NewAnimationSet()
-	PlayerAnimations.AddAnimation("idle", *libs.NewAnimation(loadImage("Characters/Knight/Knight-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
-	PlayerAnimations.AddAnimation("walk", *libs.NewAnimation(loadImage("Characters/Knight/Knight-Walk.png"), libs.FrameData{FrameCount: 4, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
+	PlayerAnimations.AddAnimation("idle", *libs.NewAnimation(LoadImage("Characters/Knight/Knight-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
+	PlayerAnimations.AddAnimation("walk", *libs.NewAnimation(LoadImage("Characters/Knight/Knight-Walk.png"), libs.FrameData{FrameCount: 4, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
 	SkeletonAnimations = *libs.NewAnimationSet()
-	SkeletonAnimations.AddAnimation("idle", *libs.NewAnimation(loadImage("Characters/Skeleton/Skeleton-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
+	SkeletonAnimations.AddAnimation("idle", *libs.NewAnimation(LoadImage("Characters/Skeleton/Skeleton-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
 	OrcAnimations = *libs.NewAnimationSet()
-	OrcAnimations.AddAnimation("idle", *libs.NewAnimation(loadImage("Characters/Orc/Orc-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
+	OrcAnimations.AddAnimation("idle", *libs.NewAnimation(LoadImage("Characters/Orc/Orc-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
 	LancerAnimations = *libs.NewAnimationSet()
-	LancerAnimations.AddAnimation("idle", *libs.NewAnimation(loadImage("Characters/Lancer/Lancer-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
+	LancerAnimations.AddAnimation("idle", *libs.NewAnimation(LoadImage("Characters/Lancer/Lancer-Idle.png"), libs.FrameData{FrameCount: 6, FrameOX: 0, FrameOY: 0, FrameWidth: 100, FrameHeight: 100, ScaleFactor: 4}))
 }
 
-func loadImage(path string) *ebiten.Image {
+func LoadImage(path string) *ebiten.Image {
 	file, err := embeddedFiles.Open("assets/" + path)
 	if err != nil {
 		log.Fatal(err)
--- internal/scenes/test_scene.go
+++ internal/scenes/test_scene.go
@@ -1,10 +1,10 @@
 package scenes
 
 import (
-	"log"
 	assets "pick-it-up"
 	"pick-it-up/internal/libs"
 	"pick-it-up/internal/objects"
+	"pick-it-up/internal/systems"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -29,22 +29,27 @@ var GroundMap = [][]int{
 
 type TestScene struct {
 	Dungeon
-	Player   objects.Player
-	Skeleton objects.Entity
+	Player           objects.Player
+	Skeleton         objects.Entity
+	CollisionHandler systems.CollsionHandler
 }
 
 func NewTestScene() *TestScene {
 	player := objects.NewPlayer(libs.Vector{X: float64(400 - (100*4)/2), Y: float64(300 - (100*4)/2)}, 50, 50, assets.AxemanAnimations)
 	skeleton := objects.NewEntity(libs.Vector{X: float64(600 - (100*4)/2), Y: float64(300 - (100*4)/2)}, 50, 50, assets.SkeletonAnimations)
+	collisionHandler := systems.NewCollisionHandler()
 
 	var tileMaps libs.TileMapSet
 	tileMaps.AddTileMap(GroundMap)
 	tileMaps.AddTileMap(TileMap)
 
+	collisionHandler.AddEntity(*skeleton)
+
 	return &TestScene{
-		Dungeon:  *NewDungeon("S", 100, tileMaps),
-		Player:   *player,
-		Skeleton: *skeleton,
+		Dungeon:          *NewDungeon("S", 100, tileMaps),
+		Player:           *player,
+		Skeleton:         *skeleton,
+		CollisionHandler: *collisionHandler,
 	}
 }
 
@@ -52,9 +57,7 @@ func (s *TestScene) Update() error {
 	s.Player.Update()
 	s.Skeleton.Update()
 
-	if s.Player.BoundingBox.Intersects(s.Skeleton.BoundingBox) {
-		log.Println("Intersection:", s.Player.Position)
-	}
+	s.CollisionHandler.Update(s.Player)
 
 	return nil
 }

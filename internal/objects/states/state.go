907e80a8848c3ae19bf895832dc3c5796e19b28c
Karxem <tippjoshua@gmx.de>
2024-07-01 22:29:44 +0200

    feat: create state type

--- internal/objects/states/state.go
+++ internal/objects/states/state.go
@@ -1,28 +1,11 @@
 package states
 
-type State interface {
-	Enter()
-	Update()
-	Exit()
-}
+type State int
 
-type StateMachine struct {
-	CurrentState State
-}
-
-func (sm *StateMachine) ChangeState(ns State) {
-	if sm.CurrentState != nil {
-		sm.CurrentState.Exit()
-	}
-
-	sm.CurrentState = ns
-	sm.CurrentState.Enter()
-}
-
-func (sm *StateMachine) Update() {
-	if sm.CurrentState == nil {
-		panic("No current state found in state machine")
-	}
-
-	sm.CurrentState.Update()
-}
+const (
+	Idle State = iota
+	Walk
+	Attack
+	Hurt
+	Dead
+)
